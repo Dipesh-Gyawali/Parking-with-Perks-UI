a3b716720e8dceb942cca4bf5b4beb48d263ad67
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-02-28 17:43:09 +0545

    feat: hover state   added

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -3,51 +3,35 @@ import "./_product.scss";
 export const Product = () => {
   return (
     <>
-    <div className="product-main-container">
-      <div className="product-container">
-        <div className="img-container">
-          <img src="./src/assets/park.png" alt="Parking Image" />
-        </div>
-        <div className="content-container">
-          <p>Location - Street Name</p>
-          <div className="table-container">
-            <table cellPadding="8" cellSpacing="20">
-              <tr className="numbers">
-                <td>20%</td>
-                <td>17</td>
-                <td>2</td>
-              </tr>
-              <tr className="characters">
-                <td>Occupancy</td>
-                <td>Parked</td>
-                <td>Warning</td>
-              </tr>
-            </table>
+      <div className="product-main-container">
+        <div className="product-container">
+          <div className="img-container">
+            <img src="./src/assets/park.png" alt="Parking Image" />
+            <div className="percentage-overlay">
+              <span>Occupancy: 20%</span>
+              <span>Parked Vehicles: 17</span>
+              <span>Location Name: 20</span>
+              <span>Warnings: 2</span>
+            </div>
+          </div>
+          <div className="content-container">
+            <p>Location - Street Name</p>
+            <div className="table-container">
+              <table cellPadding="8" cellSpacing="20">
+                <tr className="numbers">
+                  <td>20%</td>
+                  <td>17</td>
+                  <td>2</td>
+                </tr>
+                <tr className="characters">
+                  <td>Occupancy</td>
+                  <td>Parked</td>
+                  <td>Warning</td>
+                </tr>
+              </table>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="product-container">
-        <div className="img-container">
-          <img src="./src/assets/park.png" alt="Parking Image" />
-        </div>
-        <div className="content-container">
-          <p>Location - Street Name</p>
-          <div className="table-container">
-            <table cellPadding="8" cellSpacing="20">
-              <tr className="numbers">
-                <td>20%</td>
-                <td>17</td>
-                <td>2</td>
-              </tr>
-              <tr className="characters">
-                <td>Occupancy</td>
-                <td>Parked</td>
-                <td>Warning</td>
-              </tr>
-            </table>
-          </div>
-        </div>
-      </div>
       </div>
     </>
   );
