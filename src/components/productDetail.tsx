8c0eb822c4e7d1c4fb5677560bc0ee85872dd5a5
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-02-29 08:42:29 +0545

    feat:  recharts  added

--- src/components/productDetail.tsx
+++ src/components/productDetail.tsx
@@ -1,40 +1,78 @@
 // Ensure correct import of useParams from react-router-dom
 import { useParams } from "react-router-dom";
 import "./_productDetail.scss";
+import { IoMdArrowBack } from "react-icons/io";
+import { PieChart, Pie, Tooltip } from "recharts";
 
 export const ProductDetail = () => {
-  // Retrieve the id parameter from the URL
   const { id } = useParams();
-  console.log("ID:", id); // Check if the id is correctly retrieved
+  console.log("ID:", id);
+
+  const data01 = [
+    { name: "Free Space", value: 5 },
+    { name: "Occupied Space", value: 20 },
+    { name: "Unknown", value: 8 },
+    { name: "Total Space", value: 33 },
+  ];
 
   return (
     <>
       <div className="product-detail-main-container">
         <div className="title">
-          <a href={`#/product/`}> --- Back</a>
+          <a href={`#/product/`}>
+            <IoMdArrowBack /> Back
+          </a>
           <p>Overview</p>
         </div>
-        <div>
-          <div className="table-container">
-            <table cellPadding="8" cellSpacing="20">
-              <tr className="numbers">
-                <td>Free Spaces</td>
-                <td>Occupies Spaces</td>
-                <td>Unknown</td>
-                <td>Total Space</td>
-              </tr>
-              <tr className="characters">
-                <td>5</td>
-                <td>20</td>
-                <td>8</td>
-                <td>33</td>
-              </tr>
-            </table>
+        <div className="main-card-container">
+          <div className="card-container">
+            <p>Free Space</p>
+            <span>5</span>
+          </div>
+          <div className="card-container">
+            <p>Occupied Spaces</p>
+            <span>20</span>
+          </div>
+          <div className="card-container">
+            <p>Unknown</p>
+            <span>8</span>
+          </div>
+          <div className="card-container">
+            <p>Total Space</p>
+            <span>33</span>
           </div>
         </div>
-        <div></div>
+        <div className="main-chart-container">
+          <div className="chart-container">
+            <p>Occupancy Status</p>
+            <div>
+              <PieChart width={400} height={400}>
+                <Pie
+                  dataKey="value"
+                  isAnimationActive={false}
+                  data={data01}
+                  cx="50%"
+                  cy="50%"
+                  outerRadius={80}
+                  fill="#8884d8"
+                  // fill="#00C399"
+                  label
+                />
+                <Tooltip />
+              </PieChart>
+            </div>
+          </div>
+          <div className="chart-content-container">
+            <p>Rules</p>
+            <ul>
+              <li>Vehicles without proper authorization are not allowed.</li>
+              <li>Parking is only permitted during specified hours. Violators may be subject to fines or towing.</li>
+              <li>Pay required parking fees using designated methods, and display proof of payment. </li>
+              <li></li>
+            </ul>
+          </div>
+        </div>
       </div>
-      ;
     </>
   );
 };
