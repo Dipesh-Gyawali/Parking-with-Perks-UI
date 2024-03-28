c274f28c51284cf39ccae20b8f0482dfdb2f7928
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-03-28 18:49:12 +0545

    fix: unnecessary feature removed

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -5,7 +5,6 @@ export const Product = () => {
   const [searchQuery, setSearchQuery] = useState(""); // State variable for search query
   const [products, setProducts] = useState([]);
 
-  const [showAddModal, setShowAddModal] = useState(false); // State variable for showing add modal
   const [newSlot, setNewSlot] = useState({
     id: "",
     imageSrc: "",
@@ -63,17 +62,6 @@ export const Product = () => {
   const filteredProducts = products.filter((product) =>
     product.location.toLowerCase().includes(searchQuery.toLowerCase())
   );
-
-  const handleAddMoreSlots = () => {
-    setShowAddModal(true);
-  };
-  const handleInputChange = (e: any) => {
-    const { name, value } = e.target;
-    setNewSlot({
-      ...newSlot,
-      [name]: value,
-    });
-  };
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
@@ -145,78 +133,7 @@ export const Product = () => {
             </div>
           </a>
         ))}
-
-        <div className="add-more-slots" onClick={handleAddMoreSlots}>
-          <div className="add">+</div>
-          <p>Add Parking Space</p>
-        </div>
       </div>
-
-      {/* Add modal */}
-      {showAddModal && (
-        <div className="modal">
-          <div className="modal-content">
-            <span className="close" onClick={() => setShowAddModal(false)}>
-              &times;
-            </span>
-            <h2>Add Parking Slot</h2>
-            <form onSubmit={handleSubmit}>
-              <label htmlFor="id">ID:</label>
-              <input
-                type="number"
-                id="id"
-                name="id"
-                value={newSlot.id}
-                onChange={handleInputChange}
-                required
-              />
-              <label htmlFor="imageSrc">Image Source:</label>
-              <input
-                type="text"
-                id="imageSrc"
-                name="imageSrc"
-                value={newSlot.imageSrc}
-                onChange={handleInputChange}
-                required
-              />
-              <label htmlFor="location">Location:</label>
-              <input
-                type="text"
-                id="location"
-                name="location"
-                value={newSlot.location}
-                onChange={handleInputChange}
-                required
-              />
-              {/* <label htmlFor="occupancy">Occupancy:</label>
-              <input
-                type="text"
-                id="occupancy"
-                name="occupancy"
-                value={newSlot.occupancy}
-                onChange={handleInputChange}
-              /> */}
-              {/* <label htmlFor="parkedVehicles">Parked Vehicles:</label>
-              <input
-                type="text"
-                id="parkedVehicles"
-                name="parkedVehicles"
-                value={newSlot.parkedVehicles}
-                onChange={handleInputChange}
-              /> */}
-              {/* <label htmlFor="warnings">Warnings:</label>
-              <input
-                type="text"
-                id="warnings"
-                name="warnings"
-                value={newSlot.warnings}
-                onChange={handleInputChange}
-              /> */}
-              <button type="submit">Add</button>
-            </form>
-          </div>
-        </div>
-      )}
     </>
   );
 };
